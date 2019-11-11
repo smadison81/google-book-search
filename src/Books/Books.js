12cc3c2d56b75af7149a7b88be5f07d10247fa34
SDMadison <sdmadison@mdanderson.org>
2019-11-11 13:55:17 -0600

    working search

---
+++ src/Books/Books.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import BookList from "../BookList";
+import SearchBox from "../SearchBox";
+import request from "superagent";
+
+class Books extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      searchField: "",
+      sort: ""
+    };
+  }
+  componentDidMount() {
+    request
+      .get("https://www.googleapis.com/books/v1/volumes")
+      .query({ q: this.state.searchField })
+      .then(data => {
+        this.setState({ books: [...data.body.items] });
+      });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    request
+      .get("https://www.googleapis.com/books/v1/volumes")
+      .query({ q: this.state.searchField })
+      .then(data => {
+        console.log(data);
+        this.setState({ books: [...data.body.items] })
+        .catch(err => {
+          // handle the error
+        });
+      });
+  };
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  handleSort = e => {
+    this.setState({ sort: e.target.value });
+  };
+
+  render() {
+    const filteredBooks = this.state.books.sort((a, b) => {
+      if (this.state.sort == "Newest") {
+        console.log("in newest");
+        return (
+          parseInt(b.volumeInfo.publishedDate.substring(0, 4)) -
+          parseInt(a.volumeInfo.publishedDate.substring(0, 4))
+        );
+      } else if (this.state.sort == "Oldest") {
+        return (
+          parseInt(a.volumeInfo.publishedDate.substring(0, 4)) -
+          parseInt(b.volumeInfo.publishedDate.substring(0, 4))
+        );
+      }
+
+      return;
+    });
+
+    return (
+      <div className="wrapper">
+        <SearchBox
+          data={this.state}
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          handleSort={this.handleSort}
+        />
+        <BookList books={filteredBooks} />
+      </div>
+    );
+  }
+}
+
+export default Books;
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+  
+    return (
+      <header>
+        <i className="fas fa-book fa-2x"></i>
+        <h1>Google Book Search!</h1>
+      </header>
+      
+    );
+}
+
+export default Header;
+
+
